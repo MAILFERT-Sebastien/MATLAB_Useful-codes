6bc0d0dea79fc1544cd80b010e884a7ae5fd6bf3
Sébastien MAILFERT <150167221+MAILFERT-Sebastien@users.noreply.github.com>
2024-03-04 10:31:57 +0100

    updates

--- Device_control/THORLABS_KINESIS_DC MOTOR CONTROLLER_FOLLOW_CALIBRATION/init_motors.m
+++ Device_control/THORLABS_KINESIS_DC MOTOR CONTROLLER_FOLLOW_CALIBRATION/init_motors.m
@@ -1,18 +1,20 @@
-function [motor1,motor2]=init_motors()
+function [motor1,motor2]=init_motors(SN_motor1,SN_motor2)
 
 motorlist=motor.listdevices;        % List connected devices
 if (~isempty(motorlist) && size(motorlist,2)==2)
+
     motor1=motor;                       % Create a motor object
-    motor1.connect(motorlist{1});   % Connect the first device in the list of devices
+    Index1 = contains(motorlist,SN_motor1);
+    motor1.connect(motorlist{Index1});   % Connect the first device in the list of devices
     motor2=motor;                       % Create a motor object
-    motor2.connect(motorlist{2});   % Connect the second device in the list of devices
+    Index2 = contains(motorlist,SN_motor2);
+    motor2.connect(motorlist{Index2});   % Connect the second device in the list of devices
     if (~motor1.ishomed)
         motor1.home;                    % Home the device
     end%if
     if (~motor2.ishomed)
         motor2.home;                    % Home the device
     end%if
-
 else
     warning('motors are not connected');
     motor1=[];
